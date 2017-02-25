776c340134151321561b8152615432b01863f80d
akhdakhz03 <akhdakhz@gmail.com>
2017-02-25 12:58:10 +0700

    add sha1sum

---
+++ week02/work02/exercise.c
@@ -0,0 +1,15 @@
+/*
+ * Name: AKHDA KHAIRUL ZADID
+ * NPM : 1606954716
+ * Class: SI Ekstensi '16
+ * this is just another line you would ignore
+ */
+
+#define LOOP  4
+
+#include <stdio.h>
+
+void main() {
+	int input = 5;
+	printf("%d times %d equals %d\n",LOOP,input,LOOP*input); //5 times 4 equals 20
+}
